504bc1dd3d42cf3472fd5246b933902d0cf85119
Pele02 <100292297+Pele02@users.noreply.github.com>
2024-11-20 21:27:49 +0200

    Content
    Added content on the landing page and Buttons for log in

--- frontend/src/components/ContentLandingPage.jsx
+++ frontend/src/components/ContentLandingPage.jsx
@@ -1,15 +1,43 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import landingPageImg from "../assets/images/landingPage.png";
+import "../styles/ContentLandingPage.css";
 
 const ContentLandingPage = () => {
   return (
     <>
-      <div class="container">
+      <div class="container-fluid">
         <div class="row">
           <div class="col">
-            <p>Left Side</p>
+            <div class="header-text">
+              <h1>Urmărește investițiile tale</h1>
+              <p>simplu și rapid cu ajutorul nostru</p>
+            </div>
+            <ul className="list-unstyled">
+              <li>Fii la curent cu performanța acțiunilor tale</li>
+              <li>Monitorizează evoluția pieței</li>
+              <li>Accesează rapoarte detaliate</li>
+            </ul>
+            <div className="row justify-content-center btn-row">
+              <div className="col-4">
+                <button
+                  type="button"
+                  className="btn btn-primary btn-lg content-page-btn"
+                >
+                  Începe acum
+                </button>
+              </div>
+              <div className="col-4">
+                <button
+                  type="button"
+                  className="btn btn-outline-secondary btn-sm content-page-btn"
+                >
+                  Loghează-te
+                </button>
+              </div>
+            </div>
           </div>
+
           <div class="col">
             <img
               src={landingPageImg}
