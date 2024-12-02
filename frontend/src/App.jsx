22c1e9d18ca2b4b3b8308db2c3b4e4b7cfac8b8f
Pele02 <100292297+Pele02@users.noreply.github.com>
2024-12-02 17:59:58 +0200

    Removed dotenv import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import dotenv from "dotenv";
-
-//Import KEYs environment
-//dotenv.config();
 
 // Import Pages
 import LandingPage from "./pages/LandingPage";
